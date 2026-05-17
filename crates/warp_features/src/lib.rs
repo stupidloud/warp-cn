6b2f0b34b5ee19118e2f70a28a1db25106590c29
Abhishek Pandya <40919306+abhishekp106@users.noreply.github.com>
2026-05-17 18:52:35 -0500

    Promote cloud mode input v2 to prod. (#11154)
    ## Description
    <!-- Please remember to add your design buddy onto the PR for review, if
    it contains any UI changes! -->
    
    WISOTT!

--- crates/warp_features/src/lib.rs
+++ crates/warp_features/src/lib.rs
@@ -947,7 +947,6 @@ pub const DOGFOOD_FLAGS: &[FeatureFlag] = &[
     FeatureFlag::CloudModeSetupV2,
     #[cfg(not(windows))]
     FeatureFlag::SshRemoteServer,
-    FeatureFlag::CloudModeInputV2,
     FeatureFlag::DragTabsToWindows,
     FeatureFlag::HandoffCloudCloud,
     FeatureFlag::SoloUserByok,
